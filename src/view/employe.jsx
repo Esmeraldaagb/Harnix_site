137eca55f55afcd7c093eb2d67d84c37640e604b
bellarmin93 <bellarmin.zinsou@orionorigin.com>
2025-01-02 21:45:47 +0100

    add

--- src/view/employe.jsx
+++ src/view/employe.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Tableau1 from '../composants/tableau1'
 import RightIcon from '../composants/righticon';
-import { FaHome, FaUser, FaSearch } from 'react-icons/fa'; // Font Awesome icons
 import { MdDelete, MdAdd } from 'react-icons/md'; // Material Icons
-
-
+import '../css/tableau.css';
 
 export default function Employee() {
     return (
@@ -12,7 +10,7 @@ export default function Employee() {
     
             <div className='style-title'>
             <h2 >Employés</h2>
-                <div>
+                <div className='icon-container'>
                     <MdDelete size={24} color="black" /> 
                     <MdAdd size={24} color="black" /> 
                 </div>
