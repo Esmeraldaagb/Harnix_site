827a3459e4a4f69afc72db9d02d8d7a5884156e1
bellarmin93 <bellarmin.zinsou@orionorigin.com>
2025-01-08 22:48:57 +0100

    responsivite

--- src/composants/header/header.js
+++ src/composants/header/header.js
@@ -1,34 +1,68 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const button_1 = __importDefault(require("../button/button"));
 const logo1_png_1 = __importDefault(require("../../assets/logo1.png"));
 const wave_svg_1 = __importDefault(require("../../assets/wave.svg"));
-// Composant Header
 const Header = () => {
-    // Tableau de navigation (exemple)
+    const [isNavOpen, setIsNavOpen] = (0, react_1.useState)(false);
     const navItems = [
         { id: 1, title: "Nos Activités", path: "/activite" },
         { id: 2, title: "Recrutement", path: "/recrutement" },
         { id: 3, title: "Nos Produits", path: "/produit" },
         { id: 4, title: "Communauté", path: "/communaute" },
     ];
-    // Fonction pour générer les éléments de navigation à partir du tableau navItems
     const renderNavItems = () => {
         return navItems.map((item) => (react_1.default.createElement("li", { key: item.id },
-            react_1.default.createElement("a", { href: item.path, className: "text-sm font-normal font-sans text-white flex justify-center mt-6 mr-6" }, item.title))));
+            react_1.default.createElement("a", { href: item.path, className: "text-sm font-normal font-sans text-white flex justify-center p-2 transition duration-300 ease-in-out hover:bg-white hover:text-black hover:shadow-lg rounded" }, item.title))));
     };
-    return (react_1.default.createElement("div", { className: "bg-cover bg-center ", style: { backgroundImage: `url(${wave_svg_1.default})` } },
-        react_1.default.createElement("header", { className: "bg-zinc-900 h-20 p-4" },
-            react_1.default.createElement("nav", { className: "flex items-center justify-between w-full" },
-                react_1.default.createElement("div", { className: 'flex items-center gap-1' },
+    return (react_1.default.createElement("div", { className: "bg-cover bg-center", style: { backgroundImage: `url(${wave_svg_1.default})` } },
+        react_1.default.createElement("header", { className: "bg-zinc-900 h-20 p-4 fixed top-0 left-0 right-0 z-50" },
+            react_1.default.createElement("nav", { className: "flex items-center justify-between w-full relative" },
+                react_1.default.createElement("div", { className: "flex items-center gap-1" },
                     react_1.default.createElement("img", { width: 35, height: 35, src: logo1_png_1.default, alt: "Logo" }),
-                    react_1.default.createElement("h2", { className: "font-serif tracking-wide text-white ml-4 uppercase text-2xl" }, "HARNIX")),
-                react_1.default.createElement("div", { className: 'flex items-center justify-center md:justify-between' },
-                    react_1.default.createElement("ul", { className: "md:flex hidden space-x-4 flex-row" }, renderNavItems()),
-                    react_1.default.createElement(button_1.default, { label: "Investir", width: '600', height: '300', className: 'p-3 text-sm font-normal font-sans bg-white rounded-md text-center font-semibold' }))))));
+                    react_1.default.createElement("h2", { className: "font-serif text-white ml-4 uppercase text-2xl" }, "HARNIX")),
+                react_1.default.createElement("button", { onClick: () => setIsNavOpen(!isNavOpen), className: "md:hidden p-3" }, isNavOpen ? (react_1.default.createElement("svg", { className: "w-6 h-6 text-white", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http:www.w3.org/2000/svg" },
+                    react_1.default.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, d: "M6 18L18 6M6 6l12 12" }))) : (react_1.default.createElement("svg", { className: "w-6 h-6 text-white", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http:www.w3.org/2000/svg" },
+                    react_1.default.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, d: "M4 6h16M4 12h16m-7 6h7" })))),
+                react_1.default.createElement("div", { className: `absolute top-20 left-0 w-full bg-zinc-900 rounded-md shadow-lg md:static md:flex md:justify-end ${isNavOpen ? 'block' : 'hidden'}` },
+                    react_1.default.createElement("ul", { className: "flex md:flex-row flex-col md:space-x-4 space-y-2 md:space-y-0 justify-end" }, renderNavItems()),
+                    react_1.default.createElement(button_1.default, { label: "Investir", width: "600", height: "300", className: "p-3 text-sm font-normal font-sans bg-white rounded-md text-center font-semibold ml-4" }))))));
 };
 exports.default = Header;
--- src/composants/header/header.tsx
+++ src/composants/header/header.tsx
@@ -1,69 +1,82 @@
-import React from 'react';
-import Button from '../button/button'
-import logo from '../../assets/logo1.png';
-import wave from '../../assets/wave.svg'
+ import React, { useState } from 'react';
+ import Button from '../button/button';
+ import logo from '../../assets/logo1.png';
+ import wave from '../../assets/wave.svg';
 
-// Définition de l'interface pour les props du composant Header
-interface HeaderProps {
-  title?: string;
-  children?:React.ReactNode;
-}
+ interface HeaderProps {
+   title?: string;
+   children?: React.ReactNode;
+ }
 
-// Composant Header
-const Header: React.FC<HeaderProps> = () => {
-  // Tableau de navigation (exemple)
-  const navItems = [
-    { id: 1, title: "Nos Activités", path: "/activite" },
-    { id: 2, title: "Recrutement", path: "/recrutement" },
-    { id: 3, title: "Nos Produits", path: "/produit" },
-    { id: 4, title: "Communauté", path: "/communaute" },
-  ];
+ const Header: React.FC<HeaderProps> = () => {
+   const [isNavOpen, setIsNavOpen] = useState(false);
 
-  // Fonction pour générer les éléments de navigation à partir du tableau navItems
-  const renderNavItems = () => {
-    return navItems.map((item) => (
-      <li key={item.id} >
-      <a href={item.path} className="text-sm font-normal font-sans text-white flex justify-center mt-6 mr-6">
-        {item.title}
-      </a>
-    </li>
-    ));
-  };
+   const navItems = [
+     { id: 1, title: "Nos Activités", path: "/activite" },
+     { id: 2, title: "Recrutement", path: "/recrutement" },
+     { id: 3, title: "Nos Produits", path: "/produit" },
+     { id: 4, title: "Communauté", path: "/communaute" },
+   ];
 
-  return (
-    <div className="bg-cover bg-center " style={{ backgroundImage: `url(${wave})` }}>
-        <header className="bg-zinc-900 h-20 p-4">
-        
-          <nav className="flex items-center justify-between w-full">
+   const renderNavItems = () => {
+     return navItems.map((item) => (
+       <li key={item.id}>
+         <a 
+           href={item.path} 
+           className="text-sm font-normal font-sans text-white flex justify-center p-2 transition duration-300 ease-in-out hover:bg-white hover:text-black hover:shadow-lg rounded"
+         >
+           {item.title}
+         </a>
+       </li>
+     ));
+   };
 
-          <div className='flex items-center gap-1'>
-             <img width={35}  height={35} src={logo} alt="Logo" />
+   return (
+     <div className="bg-cover bg-center" style={{ backgroundImage: `url(${wave})` }}>
+       <header className="bg-zinc-900 h-20 p-4 fixed top-0 left-0 right-0 z-50">
+         <nav className="flex items-center justify-between w-full relative">
 
-              <h2 className="font-serif tracking-wide text-white ml-4 uppercase text-2xl">HARNIX</h2>
-          </div>
+           {/* Conteneur du logo et du titre */}
+           <div className="flex items-center gap-1">
+             <img width={35} height={35} src={logo} alt="Logo" />
+             <h2 className="font-serif text-white ml-4 uppercase text-2xl">HARNIX</h2>
+           </div>
 
-          <div className='flex items-center justify-center md:justify-between'>
-              <ul className="md:flex hidden space-x-4 flex-row"> 
-                {renderNavItems()}
-              </ul>
-
-              <Button 
-                label="Investir" 
-                width='600' 
-                height='300' 
-                className='p-3 text-sm font-normal font-sans bg-white rounded-md text-center font-semibold'
-              />
-         </div>
-
-         
-
+           {/* Bouton pour ouvrir ou fermer le menu déroulant */}
+           <button onClick={() => setIsNavOpen(!isNavOpen)} className="md:hidden p-3">
+             {isNavOpen ? (
+               
+               <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http:www.w3.org/2000/svg">
+                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+               </svg>
+             ) : (
              
-          </nav>
+               <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http:www.w3.org/2000/svg">
+                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+               </svg>
+             )}
+           </button>
 
+           {/* Menu de navigation */}
+           <div
+             className={`absolute top-20 left-0 w-full bg-zinc-900 rounded-md shadow-lg md:static md:flex md:justify-end ${
+               isNavOpen ? 'block' : 'hidden'
+             }`}
+           >
+             <ul className="flex md:flex-row flex-col md:space-x-4 space-y-2 md:space-y-0 justify-end">
+               {renderNavItems()}
+             </ul>
+             <Button 
+               label="Investir" 
+               width="600" 
+               height="300" 
+               className="p-3 text-sm font-normal font-sans bg-white rounded-md text-center font-semibold ml-4"
+             />
+           </div>
+         </nav>
+       </header>
+     </div>
+   );
+ };
 
-        </header>
-    </div>
-  );
-};
-
-export default Header;
+ export default Header;
